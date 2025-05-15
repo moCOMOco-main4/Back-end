292db3789dbfeca25a5af7e4d5df5eaaaca5c9c2
JunHo-L <wnsgh4368@naver.com>
2025-05-15 17:21:37 +0900

    fix/websocket라우팅

--- moCOMOco/config/asgi.py
+++ moCOMOco/config/asgi.py
@@ -6,12 +6,15 @@ django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from apps.chat.middleware import JWTAuthMiddleware
-import apps as chat_routing
+from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-  "http":      get_asgi_application(),
-  "websocket": JWTAuthMiddleware(
-      URLRouter(chat_routing.websocket_urlpatterns)
-  ),
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
