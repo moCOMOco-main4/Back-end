0e552ab166d57afb9e5bbc7a0c727bb6f4dbf46c
workface <with.you70@gmail.com>
2025-05-15 12:57:25 +0900

    Revert "Feat/manage env switch"

--- moCOMOco/manage.py
+++ moCOMOco/manage.py
@@ -2,16 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from pathlib import Path
-from dotenv import load_dotenv
 
-# .env 파일 로드 (manage.py 기준 상위 디렉토리 위치)
-load_dotenv(Path(__file__).resolve().parent / ".env")
 
 def main():
     """Run administrative tasks."""
-    env = os.getenv("DJANGO_ENV", "dev")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{env}")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -21,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
     main()
