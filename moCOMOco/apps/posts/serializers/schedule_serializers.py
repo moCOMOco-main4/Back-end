65e66443be247bd26aac80bf6978bab36d5fef3f
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-05-18 13:31:05 +0900

    feat(posts): 모집글 참여자 정보 및 인원 수 조회 필드 추가

--- moCOMOco/apps/posts/serializers/schedule_serializers.py
+++ moCOMOco/apps/posts/serializers/schedule_serializers.py
@@ -15,6 +15,19 @@ class ScheduleUpdateSerializer(serializers.ModelSerializer):
 
 
 class ScheduleListSerializer(serializers.ModelSerializer):
+    post_id = serializers.IntegerField(source='id', read_only=True)
+    post_title = serializers.CharField(source='title', read_only=True)
+    post_data = serializers.DateTimeField(source='data', read_only=True)
+
     class Meta:
         model = Schedule
-        fields = ['id', 'post', 'date', 'memo', 'created_at']
+        fields = [
+            'id',
+            'post',
+            'date',
+            'memo',
+            'created_at',
+            'post_id',
+            'post_data',
+            'post_title'
+        ]
